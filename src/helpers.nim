e8c5927f863c1b1686b263ff17a9a2595b54235c
wick3dr0se <wick3dr0se@protonmail.com>
2024-03-30 21:22:48 -0500

    implement send wrapper

--- src/helpers.nim
+++ src/helpers.nim
@@ -1,4 +1,8 @@
-import std/[strutils, times]
+import std/[asyncdispatch, strutils, times]
+import ./common
+
+proc send*(c: Client, msg: string) {.async.} =
+    await c.socket.send(msg & "\n\r")
 
 proc getEpoch*(): int =
     return parseInt(split($epochTime(), ".")[0])
